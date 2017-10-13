f5a8a7034cac5996ad00857de25649fd56e66f2a
Tobias Reinhardt <t.reinhardt@ptsgroup.de>
2017-10-13 17:04:30 +0200

    Port geändert

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
 });
